42185b426f46d3427ffe11977d39cb8aab0d1943
Sparsh Gupta <sparshgpt18@gmail.com>
2021-06-12 09:11:19 +0530

    Add files via upload

---
+++ Codeforces/Array Reordering.cpp
@@ -0,0 +1,91 @@
+// https://codeforces.com/contest/1535/problem/B
+/*
+
+     ░██████╗██████╗░░█████╗░██████╗░░██████╗██╗░░██╗  ░██████╗░██╗░░░██╗██████╗░████████╗░█████╗░
+     ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔════╝██║░░██║  ██╔════╝░██║░░░██║██╔══██╗╚══██╔══╝██╔══██╗
+     ╚█████╗░██████╔╝███████║██████╔╝╚█████╗░███████║  ██║░░██╗░██║░░░██║██████╔╝░░░██║░░░███████║
+     ░╚═══██╗██╔═══╝░██╔══██║██╔══██╗░╚═══██╗██╔══██║  ██║░░╚██╗██║░░░██║██╔═══╝░░░░██║░░░██╔══██║
+     ██████╔╝██║░░░░░██║░░██║██║░░██║██████╔╝██║░░██║  ╚██████╔╝╚██████╔╝██║░░░░░░░░██║░░░██║░░██║
+     ╚═════╝░╚═╝░░░░░╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝  ░╚═════╝░░╚═════╝░╚═╝░░░░░░░░╚═╝░░░╚═╝░░╚═╝
+    ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+    █████╗█████╗█████╗█████╗█████╗█████╗█████╗█████╗█████╗█████╗█████╗█████╗█████╗█████╗█████╗█████╗
+    ╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝╚════╝
+    ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fi          first
+#define se          second
+#define pb          push_back
+#define mp          make_pair
+#define all(x)      x.begin(), x.end()
+#define ll          long long
+#define pll         pair<long, long>
+#define vll         vector<long long>
+#define inf         1e18
+#define gcd(a, b)   __gcd(a, b)
+#define range(a,b)  substr(a,b-a+1)
+#define fori(a, n)  for (ll i = a; i < n; i++)
+#define forj(a, n)  for (ll j = a; j < n; j++)
+#define fork(a, n)  for (ll k = a; k < n; k++)
+#define print(x)    for (auto i : x) {cout << i << " ";}
+#define FIO         ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+
+template <class T>
+bool comp(T a, T b) {
+    if (a < b)
+        return true;
+    return false;
+}
+
+int main()
+{
+    FIO;
+
+    #ifndef ONLINE_JUDGE
+    //remove this piece of code when this has to be submitted in kickstart, coding ninjas
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    //freopen is used to associate a file with stdin or stdout stream in C++
+    #endif
+
+    ll t;
+    cin >> t;
+    while (t--) {
+        ll n, temp;
+        cin >> n;
+        vll arr, earr, oarr;
+        fori(0, n) {
+            cin >> temp;
+            if (temp % 2)
+                oarr.pb(temp);
+            else
+                earr.pb(temp);
+        }
+        fori(0, earr.size())
+            arr.pb(earr[i]);
+        fori(0, oarr.size())
+            arr.pb(oarr[i]);
+        ll ans = 0;
+        fori(0, n - 1) {
+            if (arr[i] == 1)
+                continue;
+            if (arr[i] % 2 == 0)
+                ans += n - i - 1;
+            else {
+                forj(i + 1, n) {
+                    if (gcd(arr[i], 2 * arr[j]) > 1)
+                        ans++;
+                }
+            }
+        }
+        cout << ans << "\n";
+        
+    }
+
+    return 0;
+}
